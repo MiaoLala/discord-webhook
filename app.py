ed0e5a1f3fed85804f5dfd2e097f4e432a23570d
MiaoLala <111435584+MiaoLala@users.noreply.github.com>
2025-06-26 11:49:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -7,5 +8,4 @@ def home():
     return "Discord Flask Web Server Running!"
 
 if __name__ == "__main__":
-    import os
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
